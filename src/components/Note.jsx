47b6dba960bc5441cfa79f8ae3069e395067eec2
Trevis Kelley <treviskelley@gmail.com>
2022-09-04 16:27:02 -0500

    Added Note component

---
+++ src/components/Note.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Note() {
+    
+    let title = "My Title";
+    let content = "My Content";
+    
+    return <div className="note">
+        <h1>{title}</h1>
+        <p>{content}</p>
+    </div>
+};
+
+export default Note;
