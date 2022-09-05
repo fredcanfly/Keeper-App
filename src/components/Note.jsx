db481b59d829d9c2fbc4ed57290b2677e055b5af
Trevis Kelley <treviskelley@gmail.com>
2022-09-04 20:48:35 -0500

    Added functionality for notes generated by array

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,13 +1,10 @@
 import React from "react";
 
-function Note() {
-    
-    let title = "My Title";
-    let content = "My Content";
-    
+function Note(props) {
+        
     return <div className="note">
-        <h1>{title}</h1>
-        <p>{content}</p>
+        <h1>{props.title}</h1>
+        <p>{props.content}</p>
     </div>
 };
 
